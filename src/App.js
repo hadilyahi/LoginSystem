c2759a0577ccfa39176f3cac921ecbcb48f75f7d
Hadil Yahi <165954236+hadilyahi@users.noreply.github.com>
2024-12-10 20:39:27 +0100

    finish the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector } from 'react-redux'; 
+import { selectUser } from './features/userSlice'; 
+import Login from './Components/Login';
+import Logout from './Components/Logout';
 
-function App() {
+const App = () => {
+  const user = useSelector(selectUser); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-0 p-0 bg-white">
+      {user ? <Logout /> : <Login />}
     </div>
   );
-}
+};
 
 export default App;
